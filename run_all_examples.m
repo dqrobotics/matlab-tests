634e12afcba14dfc0bdbe7fc542d07ea38189984
Bruno Vilhena Adorno <adorno@ufmg.br>
2020-04-17 12:51:27 -0300

    Added documentation to the run_all_examples.m

--- run_all_examples.m
+++ run_all_examples.m
@@ -1,3 +1,29 @@
+% RUN_ALL_EXAMPLES - Runs all examples in DQ Robotics and report the ones that fail.
+% 
+% USAGE: run_all_examples() or run_all_examples(start,end)'
+
+% (C) Copyright 2011-2020 DQ Robotics Developers
+% 
+% This file is part of DQ Robotics.
+% 
+%     DQ Robotics is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     DQ Robotics is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU Lesser General Public License for more details.
+% 
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+%
+% DQ Robotics website: dqrobotics.github.io
+%
+% Contributors to this file:
+%     Bruno Vihena Adorno - adorno@ufmg.br
+
 function [fail_count, fail_log] = run_all_examples(varargin)
     
     file = {'ax18_kinematic_control',...
