8a7575b5327823b55b851c43744248618b5b4805
juanjqo <juanjqo@pop-os.localdomain>
2022-01-09 00:48:17 -0300

    [get_line_from_dq] Fixed bug in input arg of the function.

--- DQ_test_facilities/get_line_from_dq.m
+++ DQ_test_facilities/get_line_from_dq.m
@@ -1,4 +1,4 @@
-function [ret] = get_line_from_dq(dq)
+function [ret] = get_line_from_dq(dq, primitive)
     dq = normalize(dq);
     ret = Ad(rotation(dq), primitive) + DQ.E * cross(translation(dq), Ad(rotation(dq), primitive));
 end
