f76264dd26897d628f68f5c6eda1e1465becd968
juanjqo <juanjqo@pop-os.localdomain>
2022-01-09 21:23:32 -0300

    [run_basic_tests] Added more tests.

--- run_basic_tests.m
+++ run_basic_tests.m
@@ -1,8 +1,26 @@
-result = runtests('DQ_TestBasicOperations')
+result = runtests('DQ_TestBasicOperations');
 
 for i = 1:length(result)
     if result(i).Failed
-       exit(1,"force");
+       exit(1,"force");      
+    end
+end
+
+
+result = runtests('DQTestCase');
+
+for i = 1:length(result)
+    if result(i).Failed
+       exit(1,"force");      
+    end
+end
+
+
+result = runtests('DQKinematicsTestCase');
+
+for i = 1:length(result)
+    if result(i).Failed
+       exit(1,"force");      
     end
 end
 exit(0)
