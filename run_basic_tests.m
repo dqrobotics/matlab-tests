6de3c437b13708a9a93366c4a48c4b44143d4832
juanjqo <juanjqo@pop-os.localdomain>
2022-01-09 21:52:13 -0300

    [run_basic_tests] Updated the script.

--- run_basic_tests.m
+++ run_basic_tests.m
@@ -1,26 +1,26 @@
-result = runtests('DQ_TestBasicOperations');
+result = runtests('DQ_TestBasicOperations')
+
+for i = 1:length(result)
+    if result(i).Failed
+       exit(1,"force");      
+    end
+end
+
+
+result = runtests('DQ_TestCase')
+
+for i = 1:length(result)
+    if result(i).Failed
+       exit(1,"force");      
+    end
+end
+
+
+result = runtests('DQ_KinematicsTestCase')
 
 for i = 1:length(result)
     if result(i).Failed
        %exit(1,"force");      
     end
 end
-
-
-result = runtests('DQ_TestCase');
-
-for i = 1:length(result)
-    if result(i).Failed
-       %exit(1,"force");      
-    end
-end
-
-
-result = runtests('DQ_KinematicsTestCase');
-
-for i = 1:length(result)
-    if result(i).Failed
-       %exit(1,"force");      
-    end
-end
-%exit(0)
+exit(0)
